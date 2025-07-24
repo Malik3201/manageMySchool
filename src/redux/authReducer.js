bff674dd7548a4c0f0d42aefe170e7ebad5ffa20
Abdul-Rehman <abdulrehmancs537@gmail.com>
2025-07-24 13:02:59 +0500

    Reapply "login and authentication added"
    This reverts commit 333886d1d06d4a11325ae748df9a01dc1889bcdf.

---
+++ src/redux/authReducer.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUsers = createAsyncThunk(
+  "fetchUsers",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const usersList = await axios.get("/data/users.json");
+      const currentUser = usersList.data.find((user) => {
+        return (
+          user.email == credentials.email &&
+          user.password == credentials.password
+        );
+      });
+      if (!currentUser) return rejectWithValue("Invalid UserName or Password");
+      return currentUser;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
+const authReducer = createSlice({
+  name: "authReducer",
+  initialState: {
+    errors: null,
+    isLoggedIn: false,
+    userId: null,
+    userRole: null,
+    isLoading: false,
+  },
+  reducers: {
+    logOut: (state) => {
+      localStorage.removeItem("userId");
+      localStorage.removeItem("userRole");
+      state.isLoggedIn = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+        state.errors = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.errors = null;
+        state.isLoggedIn = true;
+        state.userId = action.payload.id;
+        localStorage.setItem("userId", JSON.stringify(action.payload.id));
+        state.userRole = action.payload.role;
+        localStorage.setItem("userRole", JSON.stringify(action.payload.role));
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errors = action.payload;
+        console.log(state.errors);
+      });
+  },
+});
+export default authReducer.reducer;
+export const { logOut } = authReducer.actions;
